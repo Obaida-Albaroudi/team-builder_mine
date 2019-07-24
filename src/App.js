5c177282849eab0e98af001046685b155ca78c4e
Obaida-Albaroudi <o.albaroudi96@gmail.com>
2019-07-24 16:06:14 -0700

    Completed everything except edit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Teammates from "./List.js";
+import Form from "./Form.js";
 import './App.css';
 
 function App() {
+  const [team, setTeam] = useState([]);
+
+  console.log(team)
+
+  const submitHandler = event =>{
+    event.preventDefault();
+    return(
+      <div>
+        <Form team={team} setTeam={setTeam} />
+      </div>
+    )
+      
+  };
+
+  // function changeState(){
+  //   i
+  // }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Form team={team} setTeam={setTeam} />
+      <h2>Teammates: </h2>
+        {team.map( mate => {
+            return (
+                <div>
+                    <p>Name: {mate.Name}</p>
+                    <p>Email: {mate.Email}</p>
+                    <p>Role: {mate.Role}</p>
+                    <form onSubmit={submitHandler}>
+                    <button type="submit">Edit</button>       
+                    </form>
+                </div>
+            )}
+        )}
     </div>
   );
 }
